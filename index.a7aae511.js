c1ba304d4bed83ad48d760536013b62e8c8b4676
BulatovaTati <102695106+BulatovaTati@users.noreply.github.com>
2022-09-27 18:09:37 +0000

    Deploying to gh-pages from @ BulatovaTati/IceCream@169ffdeeb1b6a2e1ee9464902b72cec77a21c0f6 🚀

--- index.a7aae511.js
+++
@@ -1,2 +0,0 @@
-function changeScrollButtonVisibility(){var t=document.querySelector(".scroll-to-top");window.pageYOffset>60?t.classList.remove("js-transparent"):t.classList.add("js-transparent")}window.onscroll=function(){return changeScrollButtonVisibility()};
-//# sourceMappingURL=index.a7aae511.js.map
